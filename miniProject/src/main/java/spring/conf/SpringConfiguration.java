d050348101d6b1b923a15fc102dc4a10a3edaaf1
KIMDONGBEEN <dd1761@naver.com>
2023-04-26 20:01:49 +0900

    23-04-26

--- miniProject/src/main/java/spring/conf/SpringConfiguration.java
+++ miniProject/src/main/java/spring/conf/SpringConfiguration.java
@@ -57,8 +57,7 @@ public class SpringConfiguration {
 				new ClassPathResource("channel/dao/channelMapper.xml"),
 				new ClassPathResource("subscribe/dao/subscribeMapper.xml"),
 
-				new ClassPathResource("like/dao/likeMapper.xml")
-
+				new ClassPathResource("like/dao/likeMapper.xml"),
 				new ClassPathResource("comment/dao/commentMapper.xml")
 
 		);
