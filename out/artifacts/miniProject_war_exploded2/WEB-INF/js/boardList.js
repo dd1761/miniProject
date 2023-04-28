23a164719944a366e0b0cfc8f6a08f4f8f007c58
ksqrt <ksqrt98@gmail.com>
2023-04-28 15:53:19 +0900

    링크수정

--- out/artifacts/miniProject_war_exploded2/WEB-INF/js/boardList.js
+++ out/artifacts/miniProject_war_exploded2/WEB-INF/js/boardList.js
@@ -44,7 +44,7 @@ $(function(){
                     <div id='box-center'>
                       <div class="channel-details">
                         <h1>&nbsp${data[0].channel_name}</h1>
-                        <p>&nbsp구독자 ${data[0].subscriber_count}명 동영상 ${comments}개</p>
+                        <p>&nbsp구독자 ${data[0].subscriber_count}명 글 ${comments}개</p>
                         <p>
                           <a href="#">&nbsp${data[0].channel_description}</a>
                         </p>
