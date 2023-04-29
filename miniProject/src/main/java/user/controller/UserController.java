1f267279e71ff1675e807b6b93b262ccf728d097
KIMDONGBEEN <dd1761@naver.com>
2023-04-29 16:52:04 +0900

    23-04-29

--- miniProject/src/main/java/user/controller/UserController.java
+++ miniProject/src/main/java/user/controller/UserController.java
@@ -14,11 +14,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 import board.bean.BoardDTO;
 import channel.bean.ChannelDTO;
@@ -194,11 +192,3 @@ public class UserController {
 		return communityService.getBoardCount(channel_id, user_id);
 	}
 }
-
-
-
-
-
-
-
-
